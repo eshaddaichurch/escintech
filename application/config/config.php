0642fb5485c01bbe0cbbc2f11281326979c94a47
tonis laptop <tonibackupfile@gmail.com>
2023-03-04 13:34:30 +0700

    update git ignore

--- application/config/config.php
+++ application/config/config.php
@@ -23,6 +23,8 @@ date_default_timezone_set('Asia/Jakarta');
 | a PHP script and you can easily do that on your own.
 |
 */
+//$config['base_url'] = 'https://escintech.com/';
+
 $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 $config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
 $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
@@ -380,7 +382,7 @@ $config['encryption_key'] = 'randomstring';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_escintech_back';
+$config['sess_cookie_name'] = 'ci_escintech_front';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
