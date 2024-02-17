486ca97174805fb47d317c4eaa0dc6f3e3452390
Toni Simanjuntak <58927171+tonisimanjuntak@users.noreply.github.com>
2024-02-17 16:12:04 +0700

    update front end 1

---
+++ application/views/template/festavalive/header.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Festava Live - Bootstrap 5 CSS Template</title>
+
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;400;700&display=swap" rel="stylesheet">
+
+    <link href="<?php echo base_url('assets/FestavaLive/') ?>css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="<?php echo base_url('assets/FestavaLive/') ?>css/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="<?php echo base_url('assets/FestavaLive/') ?>css/templatemo-festava-live.css" rel="stylesheet">
+    
+    
+    <style>
+        
+        
+
+    /*** Navbar ***/
+
+
+    @media (max-width: 991.98px) {
+        .navbar .navbar-nav {
+            margin-top: 10px;
+            border-top: 1px solid rgba(255, 255, 255, .3);
+            background: var(--dark);
+        }
+
+        .navbar .navbar-nav .nav-link {
+            padding: 10px 0;
+        }
+    }
+
+    @media (min-width: 992px) {
+        .navbar .nav-item .dropdown-menu {
+            display: block;
+            visibility: hidden;
+            top: 100%;
+            transform: rotateX(-75deg);
+            transform-origin: 0% 0%;
+            transition: .5s;
+            opacity: 0;
+        }
+
+        .navbar .nav-item:hover .dropdown-menu {
+            transform: rotateX(0deg);
+            visibility: visible;
+            transition: .5s;
+            opacity: 1;
+        }
+    }
+
+
+    </style>
+    <!--
+
+TemplateMo 583 Festava Live
+
+https://templatemo.com/tm-583-festava-live
+
+-->
+</head>
---
+++ application/views/template/festavalive/topmenu.php
@@ -0,0 +1,146 @@
+<nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="index.html">
+                    Festava Live
+                </a>
+
+                <a href="ticket.html" class="btn custom-btn d-lg-none ms-auto me-4">Buy Ticket</a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav align-items-lg-center ms-auto me-lg-5">
+
+                        <?php
+                            $rsMenu1 = $this->db->query("SELECT * FROM V_frontmenus WHERE levels=1 ORDER BY nomorurut") ;
+                            if ($rsMenu1->num_rows()>0) {
+                                foreach ($rsMenu1->result() as $row1) {
+
+                                    $urlmenu = '';
+                                    if ($row1->openinnewtab=='1') {
+                                        $openinnewtab = ' target="_blank" ';
+                                    }else{
+                                        $openinnewtab = '';
+                                    }
+
+                                    switch ($row1->jenismenu) {
+                                        case 'Url Link':
+                                            if (substr($row1->linkmenu, 0,1)=='/') {
+                                                $urlmenu = site_url($row1->linkmenu.'/'.$this->encrypt->encode($row1->idmenu)); // http://localhost/escintech//ourlocation/index
+                                            }else{
+                                                $urlmenu = $row1->linkmenu;
+                                            }
+                                            break;
+                                        case 'Kategori Link':
+                                                $urlmenu = site_url('kategori/index/'.$this->encrypt->encode($row1->idmenu).'/'.$row1->slug_pageskategori);
+                                                break;
+                                        case 'Page Link':
+                                                $urlmenu = site_url('pages/read/'.$this->encrypt->encode($row1->idmenu).'/'.$row1->slug_pages);
+                                                break;
+                                        default:
+                                            $urlmenu = "#";
+                                            break;
+                                    }
+
+                                    
+                                    $rsMenu2 = $this->db->query("SELECT * FROM V_frontmenus WHERE parentidmenu='".$row1->idmenu."' ORDER BY nomorurut") ;
+                                    if ($rsMenu2->num_rows()>0) {
+
+                                        
+                                        //ada turunan menu / dropdown
+                                        echo '
+                                        <li class="nav-item dropdown">
+                                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">'.$row1->namamenu.'</a>
+                                        <div class="dropdown-menu m-0">';
+                                        
+                                        foreach ($rsMenu2->result() as $row2) {
+                                            if ($row2->openinnewtab=='1') {
+                                                $openinnewtab = ' target="_blank" ';
+                                            }else{
+                                                $openinnewtab = '';
+                                            }
+                                            $urlmenu = '';
+                                            switch ($row2->jenismenu) {
+                                                case 'Url Link':
+                                                    if (substr($row2->linkmenu, 0,1)=='/') {
+                                                        $urlmenu = site_url($row2->linkmenu.'/'.$this->encrypt->encode($row2->idmenu));
+                                                    }else{
+                                                        $urlmenu = $row2->linkmenu;
+                                                    }
+                                                    break;
+                                                case 'Kategori Link':
+                                                        $urlmenu = site_url('kategori/index/'.$this->encrypt->encode($row2->idmenu).'/'.$row2->slug_pageskategori);
+                                                        break;
+                                                case 'Page Link':
+                                                        $urlmenu = site_url('pages/read/'.$this->encrypt->encode($row2->idmenu).'/'.$row2->slug_pages);
+                                                        break;
+                                                default:
+                                                    $urlmenu = "#";
+                                                    break;
+                                            }
+
+                                            echo '<a href="'.$urlmenu.'" class="dropdown-item"'.$openinnewtab.'>'.$row2->namamenu.'</a>';
+
+                                        }
+
+                                        echo '
+                                                </div>
+                                            </li>
+                                        ';
+
+                                    }else{
+                                        //tidak punya turunan menu
+                                        echo '
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="'.$urlmenu.'"'.$openinnewtab.'>'.$row1->namamenu.'</a>
+                                            </li>
+                                        ';
+
+                                    }
+
+
+                                }
+                            }
+                        ?>
+
+                        <!-- <li class="nav-item">
+                            <a class="nav-link click-scroll" href="#section_1">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="#section_2">About</a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                            <div class="dropdown-menu m-0">
+                                <a href="team.html" class="dropdown-item">Our Team</a>
+                                <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                                <a href="404.html" class="dropdown-item">404 Page</a>
+                            </div>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="#section_3">Artists</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="#section_4">Schedule</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="#section_5">Pricing</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="#section_6">Contact</a>
+                        </li> -->
+                    </ul>
+
+                    <a href="ticket.html" class="btn custom-btn d-lg-block d-none">Buy Ticket</a>
+                </div>
+            </div>
+        </nav>
