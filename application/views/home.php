16da8424ccbd61aaa25312cd5d6fab4b576c70b8
Rivan Stevanus <129915027+Rivan8@users.noreply.github.com>
2024-05-11 11:10:31 +0700

    maps

--- application/views/home.php
+++ application/views/home.php
@@ -1247,7 +1247,7 @@
 
         <div class="row g-0">
           <div class="col-lg-6 venue-map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0" allowfullscreen></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.816887503385!2d109.31588607555766!3d-0.04855789995095277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e1d591202901daf%3A0x7138b14058c20347!2sGBI%20El%20Shaddai!5e0!3m2!1sid!2sid!4v1715400581015!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
 
           <div class="col-lg-6 venue-info">
