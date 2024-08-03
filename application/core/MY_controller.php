86a3143b1dba911f1f7d0947e4890f21d745cb50
Toni Simanjuntak <58927171+tonisimanjuntak@users.noreply.github.com>
2024-08-03 10:53:54 +0700

    Update MY_controller.php

--- application/core/MY_controller.php
+++ application/core/MY_controller.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class MY_controller extends CI_Controller
+class MY_Controller extends CI_Controller
 {
 
     public function isLogin()
