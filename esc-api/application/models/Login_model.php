3375f458c39832b17b33625e5903e92303385ed1
Toni Simanjuntak <58927171+tonisimanjuntak@users.noreply.github.com>
2025-04-11 12:22:54 +0700

    Update Login_model.php

--- esc-api/application/models/Login_model.php
+++ esc-api/application/models/Login_model.php
@@ -1,14 +1,14 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login_model extends CI_Model {
+class Login_model extends CI_Model
+{
 
 	public function ceklogin($email, $password)
 	{
 		$rsTemp = $this->db->query("
-				select * from jemaat where email='$email' and password = '".md5($password)."'
+				select * from v_jemaat where email='$email' and password = '" . md5($password) . "'
 			");
 		return $rsTemp;
-	}	
-
+	}
 }
